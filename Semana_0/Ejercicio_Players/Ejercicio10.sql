e06d40341f73315e6513a03ff9d0f65441db7f27
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-26 11:44:37 -0400

    Add files via upload
    Ejercicios relacionados a la tarea de players con la base de datos utilizada

---
+++ Semana_0/Ejercicio_Players/Ejercicio10.sql
@@ -0,0 +1,2 @@
+SELECT "first_name", "last_name", "height" AS 'Tallest Player NY'
+WHERE "birth_state" = 'NY' ORDER BY "height" DESC;
---
+++ Semana_0/Ejercicio_Players/Ejercicio2.sql
@@ -0,0 +1,3 @@
+SELECT "bats" FROM "players"
+WHERE "firts_name" = 'Babe'
+AND "last_name" = 'Ruth' ;
