d1e692952588f40287984eb163a5254a7f4c9484
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:48:33 -0400

    Update Ejercicio1.sql

--- Semana_0/Ejercicio_Players/Ejercicio1.sql
+++ Semana_0/Ejercicio_Players/Ejercicio1.sql
@@ -1,3 +1,5 @@
+-- 1.sql: Encuentra la ciudad natal de Jackie Robinson (incluyendo ciudad, estado y país).
+
 SELECT "birth_city", "birth_state", "birth_country"
 FROM "players" WHERE "firts_name" = 'Jackie'
 AND "last_name" = 'Robinson';
