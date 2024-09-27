59f542735f6daca571c56842537a313487f8623b
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:29:54 -0400

    Update Ejercicio2.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio2.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio2.sql
@@ -1,3 +1,12 @@
+--## 2.sql
+--Tu gerente general (es decir, la persona que toma decisiones sobre los contratos de los jugadores)
+--te pregunta si el equipo debería cambiar a un jugador actual por Cal Ripken Jr., 
+--una estrella que probablemente esté cerca de su retiro. En `2.sql`, 
+--escribe una consulta SQL para encontrar el historial salarial de Cal Ripken Jr.
+-- Ordena por año en orden descendente.
+-- Tu consulta debe devolver una tabla con dos columnas: una para el año y otra para el salario.
+
+
 SELECT "salaries"."year", "salaries"."salary"
 FROM "salaries" JOIN "players"
 ON "players"."id" = "salaries"."player_id"
