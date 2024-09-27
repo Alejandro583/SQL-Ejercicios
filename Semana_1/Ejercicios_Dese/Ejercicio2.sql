00ff13c0b4221e187ddb28a3599ed00b7da4c078
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 09:59:57 -0400

    Update Ejercicio2.sql
    Descripcion del problema

--- Semana_1/Ejercicios_Dese/Ejercicio2.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio2.sql
@@ -1,2 +1,7 @@
+--## 2.sql
+--Tu equipo está trabajando en archivar datos antiguos. En `2.sql`, escribe una consulta SQL para encontrar 
+--los nombres de los distritos que ya no están operativos.
+--Los distritos que ya no están operativos tienen “(non-op)” al final de su nombre.
+
 SELECT "name" FROM "districts"
 WHERE "name" LIKE '%non-op%';
