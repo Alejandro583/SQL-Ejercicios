98afe206070c93a2457ecd65a508538fb700de1a
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 15:08:23 -0400

    Update Ejercicio11.sql

--- Semana_0/Ejercicios_Cyberchase/Ejercicio11.sql
+++ Semana_0/Ejercicios_Cyberchase/Ejercicio11.sql
@@ -1,2 +1,4 @@
+-- ejercicio11: Consultas que enumeran títulos de episodios de la temporada 5 en orden alfabético inverso.
+
 SELECT "title" FROM "episodes"
 WHERE "season" = 5  ORDER BY "title" DESC;
