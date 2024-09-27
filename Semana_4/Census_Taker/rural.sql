1a9fd0279509ef93c24e42dac76a47ef33cda66b
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 13:43:08 -0400

    Update rural.sql

--- Semana_4/Census_Taker/rural.sql
+++ Semana_4/Census_Taker/rural.sql
@@ -1,3 +1,9 @@
+-- En rural.sql, escribe una declaración SQL para crear una vista llamada rural. 
+-- Esta vista debe contener todos los registros censales relacionados con un municipio rural 
+-- (identificado por incluir "rural" en su nombre). 
+-- Asegúrate de que la vista contenga todas las columnas de la tabla census.
+
+
 CREATE VIEW "rural" AS
 SELECT * FROM "census"
 WHERE "locality" LIKE '%rural%';
