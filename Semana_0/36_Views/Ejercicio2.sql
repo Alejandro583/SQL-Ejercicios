a5ebf142179f3c6ec0e11ba056e35eb219c7ac5c
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:54:22 -0400

    Update Ejercicio2.sql

--- Semana_0/36_Views/Ejercicio2.sql
+++ Semana_0/36_Views/Ejercicio2.sql
@@ -1,3 +1,5 @@
+-- 2.sql: Enumera los colores promedio de las impresiones de Hokusai que contienen "river" en el título en inglés.
+
 SELECT "average_color" FROM "views"
 WHERE "english_title" LIKE '%river%'
 AND "artist" = 'Hokusai';
