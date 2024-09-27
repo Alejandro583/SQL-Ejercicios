47400295adf14c0b9fbe6bf5e34dab2ef5f194b2
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 11:08:00 -0400

    Update Ejercicio12.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio12.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio12.sql
@@ -1,3 +1,13 @@
+--## 12.sql
+--Los hits son geniales, ¡pero también lo son los RBIs! En `12.sql`, escribe una consulta 
+--SQL para encontrar a los jugadores entre los 10 menos costosos por hit y entre los 10 menos costosos por RBI en 2001.
+
+-- Tu consulta debe devolver una tabla con dos columnas: una para el primer nombre del jugador y otra para su apellido.
+-- Puedes calcular el salario por RBI de un jugador dividiendo su salario de 2001 por su número de RBIs en 2001.
+-- Puedes suponer, para simplificar, que un jugador solo tendrá un salario y una actuación en 2001.
+-- Ordena tus resultados por el ID del jugador, de menor a mayor (o alfabéticamente por apellido, ya que ambos son iguales en este caso).
+-- ¡Ten en cuenta las lecciones que aprendiste en `10.sql` y `11.sql`!
+
 SELECT
     "players"."first_name",
     "players"."last_name"
@@ -13,6 +23,6 @@ WHERE
     AND "performances"."year" = 2001
     AND "performances"."RBI" != 0
 ORDER BY
-    ("salaries"."salary" / "performances"."H") , -- Ordenar por salario por golpe
-    ("salaries"."salary" / "performances"."RBI") -- Luego ordenar por salario por RBI
+    ("salaries"."salary" / "performances"."H") ,
+    ("salaries"."salary" / "performances"."RBI");
 
