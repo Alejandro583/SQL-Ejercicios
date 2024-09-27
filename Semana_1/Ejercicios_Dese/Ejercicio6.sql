141ad473d95bf74e3fefb59a7cd19b4125bf015c
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:04:50 -0400

    Update Ejercicio6.sql
    Descripción del problema

--- Semana_1/Ejercicios_Dese/Ejercicio6.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio6.sql
@@ -1,3 +1,8 @@
+--## 6.sql
+--DESE quiere evaluar qué escuelas lograron una tasa de graduación del 100%. En `6.sql`,
+--escribe una consulta SQL para encontrar los nombres de las escuelas (¡públicas o charter!) 
+--que informaron una tasa de graduación del 100%.
+
 SELECT "schools"."name" FROM "schools"
 JOIN "graduation_rates"
 ON "schools".id = "graduation_rates"."school_id"
