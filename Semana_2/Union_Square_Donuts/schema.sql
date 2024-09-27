21eb465f17b07d2a406764ce7a5fcb4852fec0b9
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 12:08:02 -0400

    Update schema.sql

--- Semana_2/Union_Square_Donuts/schema.sql
+++ Semana_2/Union_Square_Donuts/schema.sql
@@ -1,3 +1,40 @@
+-- Para entender las expectativas del equipo para su base de datos, 
+-- te sentaste a hablar con ellos después de que la tienda cerrara por el día.
+
+-- Ingredientes
+-- Sin duda necesitamos llevar un registro de nuestros ingredientes. 
+-- Algunos de los ingredientes típicos que usamos incluyen harina, levadura, 
+-- aceite, mantequilla y varios tipos diferentes de azúcar. 
+-- Además, nos encantaría llevar un registro del precio que pagamos por unidad 
+-- de ingrediente (ya sea en libras, gramos, etc.).
+
+-- Donas
+-- Necesitaremos incluir nuestra selección de donas, ¡pasadas y presentes! 
+-- Para cada dona en el menú, nos gustaría incluir tres cosas:
+-- - El nombre de la dona
+-- - Si la dona es libre de gluten
+-- - El precio por dona
+-- Oh, y es importante que podamos consultar los ingredientes de cada una de las donas!
+
+-- Pedidos
+-- Nos encanta ver a los clientes en persona, aunque nos damos cuenta 
+-- de que un buen número de personas podrían pedir en línea hoy en día. 
+-- Nos gustaría poder llevar un registro de esos pedidos en línea. 
+-- Creemos que necesitaríamos almacenar:
+-- - Un número de pedido, para llevar un registro de cada pedido internamente
+-- - Todas las donas en el pedido
+-- - El cliente que realizó el pedido. Supongamos que podríamos asumir 
+-- que solo un cliente realiza un pedido dado.
+
+-- Clientes
+-- Oh, y nos damos cuenta de que sería encantador llevar un registro 
+-- de alguna información sobre cada uno de nuestros clientes. 
+-- Nos encantaría recordar la historia de los pedidos que han realizado. 
+-- En ese caso, creemos que deberíamos almacenar:
+-- - El nombre y apellido de un cliente
+-- - Un historial de sus pedidos
+
+
 CREATE TABLE "ingredientes"(
     "id" NUMERIC,
     "ingrediente" TEXT,
