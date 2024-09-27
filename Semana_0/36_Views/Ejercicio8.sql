5120e67bb6af4a531d4a8cb0722ce849c9583f1b
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:56:19 -0400

    Update Ejercicio8.sql

--- Semana_0/36_Views/Ejercicio8.sql
+++ Semana_0/36_Views/Ejercicio8.sql
@@ -1,3 +1,5 @@
+-- 8.sql: Enumera los títulos en inglés de las 5 impresiones con el menor contraste de Hokusai.
+
 SELECT "english_title" FROM "views"
 WHERE "artist" = 'Hokusai'
 ORDER BY "contrast" LIMIT 5;
