2332a981416ba253450014ccc5ae86cf26749b09
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:08:22 -0400

    Update Ejercicio10.sql

--- Semana_1/Ejercicios_Dese/Ejercicio10.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio10.sql
@@ -1,3 +1,10 @@
+--## 10.sql
+--En Massachusetts, los gastos de los distritos escolares se determinan en parte por los impuestos
+--locales sobre el valor de las propiedades (por ejemplo, casas). En `10.sql`, 
+--escribe una consulta SQL para encontrar los 10 distritos escolares públicos con el mayor gasto por estudiante.
+--Tu consulta debe devolver los nombres de los distritos y el gasto por estudiante en cada uno.
+
+
 SELECT "districts"."name","expenditures"."per_pupil_expenditure"
 FROM "districts"
 JOIN "expenditures"
