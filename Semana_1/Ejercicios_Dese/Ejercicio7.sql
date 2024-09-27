814cf8556914ebcab14a8424686eb382441972c7
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:05:54 -0400

    Update Ejercicio7.sql
    Descripción del problema

--- Semana_1/Ejercicios_Dese/Ejercicio7.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio7.sql
@@ -1,3 +1,9 @@
+--## 7.sql
+--DESE está preparando un informe sobre las escuelas en el distrito escolar de Cambridge. 
+--En `7.sql`, escribe una consulta SQL para encontrar los nombres de las escuelas 
+--(¡públicas o charter!) en el distrito escolar de Cambridge. Ten en cuenta que Cambridge,
+--la ciudad, contiene algunos distritos escolares, pero DESE está interesada en el distrito cuyo nombre es “Cambridge”.
+
 SELECT "schools"."name" FROM "schools"
 JOIN "districts"
 ON "schools"."district_id" = "districts"."id"
