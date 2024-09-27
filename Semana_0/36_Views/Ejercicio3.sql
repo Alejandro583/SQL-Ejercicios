1ed50f0f8a16e9d8c304a5070253c7563e92b9b9
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:54:40 -0400

    Update Ejercicio3.sql

--- Semana_0/36_Views/Ejercicio3.sql
+++ Semana_0/36_Views/Ejercicio3.sql
@@ -1,3 +1,5 @@
+-- 3.sql: Cuenta cuántas impresiones de Hokusai incluyen "Fuji" en el título en inglés.
+
 SELECT COUNT("english_title") FROM "views"
 WHERE "english_title" LIKE '%Fuji%'
 AND "artist" = 'Hokusai';
