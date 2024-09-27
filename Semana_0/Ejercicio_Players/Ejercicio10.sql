6db76da77cbc344d17c6b1f6a68010756545bf87
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:52:27 -0400

    Update Ejercicio10.sql

--- Semana_0/Ejercicio_Players/Ejercicio10.sql
+++ Semana_0/Ejercicio_Players/Ejercicio10.sql
@@ -1,2 +1,6 @@
+-- 10.sql: Escribe una consulta SQL para responder una pregunta de tu elección,
+-- utilizando AS para renombrar una columna, al menos una condición 
+-- WHERE y ordenando por al menos una columna.
+
 SELECT "first_name", "last_name", "height" AS 'Tallest Player NY'
 WHERE "birth_state" = 'NY' ORDER BY "height" DESC;
