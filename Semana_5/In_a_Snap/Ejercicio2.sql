f503f98f30aaf9b528c5bd2a017c9378192ea32e
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:22:48 -0400

    Update Ejercicio2.sql

--- Semana_5/In_a_Snap/Ejercicio2.sql
+++ Semana_5/In_a_Snap/Ejercicio2.sql
@@ -1,2 +1,7 @@
+-- Los usuarios deben ser prevenidos de reabrir un mensaje que ha expirado. 
+-- Encuentra cuándo expira el mensaje con ID 151. Puedes usar directamente el ID del mensaje en tu consulta.
+
+-- Asegúrate de que tu consulta utilice el índice que se crea automáticamente en la columna de clave primaria de la tabla messages.
+
 SELECT "expires_timestamp" FROM "messages"
 WHERE "id" = 151;
