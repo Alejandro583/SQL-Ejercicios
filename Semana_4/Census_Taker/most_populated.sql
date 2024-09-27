487e2da959372795e6d0e5c3795c16d4412db1a2
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 13:46:10 -0400

    Update most_populated.sql

--- Semana_4/Census_Taker/most_populated.sql
+++ Semana_4/Census_Taker/most_populated.sql
@@ -1,3 +1,15 @@
+-- En most_populated.sql, escribe una declaración SQL para crear una vista llamada most_populated. 
+-- Esta vista debe contener, en orden de mayor a menor, los distritos más poblados de Nepal. 
+-- Asegúrate de que la vista contenga cada una de las siguientes columnas:
+
+-- district, que es el nombre del distrito.
+-- families, que es el total de familias en el distrito.
+-- households, que es el total de hogares en el distrito.
+-- population, que es la población total del distrito.
+-- male, que es el total de personas que se identifican como hombres en el distrito.
+-- female, que es el total de personas que se identifican como mujeres en el distrito.
+
+
 CREATE VIEW "most_populated" AS
 SELECT "district",SUM("families") AS "families"
 ,SUM("households") AS "households"
