d4c48fc97f9b08e14390132d233c19052d9cc3f3
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 13:32:36 -0400

    Update one_bedrooms.sql

--- Semana_4/Bed_and_Breakfast/one_bedrooms.sql
+++ Semana_4/Bed_and_Breakfast/one_bedrooms.sql
@@ -1,3 +1,14 @@
+-- En one_bedrooms.sql, escribe una declaración SQL para crear una vista llamada one_bedrooms.
+-- Esta vista debe contener todas las listas que tienen exactamente una habitación.
+-- Asegúrate de que la vista contenga las siguientes columnas:
+
+-- id, que es el ID de la lista de la tabla listings.
+-- property_type, de la tabla listings.
+-- host_name, de la tabla listings.
+-- accommodates, de la tabla listings.
+
+
+
 CREATE VIEW "one_bedrooms" AS
 SELECT "id","property_type","host_name","accommodates"
 FROM "listings"
