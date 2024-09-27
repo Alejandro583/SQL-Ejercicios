90bee8d3d4f9b92794180598b28a0a0e3550d091
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 13:37:07 -0400

    Update no_descriptions.sql

--- Semana_4/Bed_and_Breakfast/no_descriptions.sql
+++ Semana_4/Bed_and_Breakfast/no_descriptions.sql
@@ -1,3 +1,11 @@
+
+--La columna description contiene descripciones 
+-- con muchos saltos de línea, cada uno de los cuales se imprime en tu terminal.
+
+-- En no_descriptions.sql, escribe una declaración SQL para crear una vista llamada no_descriptions 
+-- que incluya todas las columnas de la tabla listings excepto description.
+
+
 CREATE VIEW "no_descriptions" AS
 SELECT "id","property_type","host_name","accommodates",
 "bedrooms" FROM "listings";
