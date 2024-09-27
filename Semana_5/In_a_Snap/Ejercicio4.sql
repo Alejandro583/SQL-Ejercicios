2f496abc9fa55c77acc365465f9668d3059e0f0d
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:24:23 -0400

    Update Ejercicio4.sql

--- Semana_5/In_a_Snap/Ejercicio4.sql
+++ Semana_5/In_a_Snap/Ejercicio4.sql
@@ -1,3 +1,8 @@
+-- La aplicación necesita enviar a los usuarios un resumen de su compromiso. 
+-- Encuentra el nombre de usuario del usuario más popular, definido como el usuario al que se le han enviado más mensajes.
+ 
+-- Asegúrate de que tu consulta utilice el índice search_messages_by_to_user_id, que se define de la siguiente manera:
+
 SELECT "username" FROM "users"
 WHERE "id" = (SELECT "to_user_id" FROM "messages"
     GROUP BY "to_user_id"
