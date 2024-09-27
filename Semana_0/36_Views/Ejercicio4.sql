a4eceec18de336871d22c662b3f9dccef1149058
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:54:57 -0400

    Update Ejercicio4.sql

--- Semana_0/36_Views/Ejercicio4.sql
+++ Semana_0/36_Views/Ejercicio4.sql
@@ -1,3 +1,5 @@
+-- 4.sql: Cuenta cuántas impresiones de Hiroshige tienen títulos en inglés que se refieren a la "Eastern Capital".
+
 SELECT COUNT("english_title") FROM "views"
 WHERE "english_title" LIKE '%Eastern Capital%'
 AND "artist" = 'Hiroshige';
