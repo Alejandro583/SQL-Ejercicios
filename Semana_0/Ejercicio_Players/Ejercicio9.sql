43716797c15d81c62a424f243ed771d815ffcede
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:52:04 -0400

    Update Ejercicio9.sql

--- Semana_0/Ejercicio_Players/Ejercicio9.sql
+++ Semana_0/Ejercicio_Players/Ejercicio9.sql
@@ -1,3 +1,6 @@
+-- 9.sql: Encuentra a los jugadores que jugaron su último juego en la MLB en 2022,
+-- ordenando alfabéticamente por nombre y luego por apellido.
+
 SELECT "first_name", "last_name" FROM "players"
 WHERE "final_game" LIKE '2022-__-__'
 ORDER BY "first_name", "last_name";
