430784303b327fdea1edace464b84bfa10f05c94
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:34:14 -0400

    Update Ejercicio6.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio6.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio6.sql
@@ -1,3 +1,12 @@
+--## 6.sql
+--¿Qué equipos podrían ser la mayor competencia para los A’s este año? En `6.sql`, 
+--escribe una consulta SQL para devolver los 5 equipos principales,
+--ordenados por el número total de hits de los jugadores en 2001.
+-- Llama a la columna que representa los hits totales en 2001 “total hits”.
+-- Ordena por hits totales, de mayor a menor.
+-- Tu consulta debe devolver dos columnas: una para el nombre de los equipos y otra para los hits totales en 2001.
+
+
 SELECT "teams"."name", SUM("performances"."H") AS 'total hits'
 FROM "teams"
 JOIN "performances"
