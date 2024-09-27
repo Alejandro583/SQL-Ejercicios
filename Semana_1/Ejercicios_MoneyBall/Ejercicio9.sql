eb1d76fe73bd8f2a1fc483f95e6b45ad1a0407ff
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:44:15 -0400

    Update Ejercicio9.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio9.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio9.sql
@@ -1,3 +1,12 @@
+--## 9.sql
+--¿Qué salarios están pagando otros equipos? En `9.sql`, escribe una consulta SQL
+--para encontrar a los 5 equipos que pagan menos (por salario promedio) en 2001.
+
+-- Redondea la columna de salario promedio a dos decimales y llámala “average salary”.
+-- Ordena a los equipos por salario promedio, de menor a mayor.
+-- Tu consulta debe devolver una tabla con dos columnas: una para el nombre de los equipos y otra para su salario promedio.
+
+
 SELECT "teams"."name", ROUND(AVG("salaries"."salary"),2)
 AS "AVG SALARY"
 FROM "teams" JOIN "salaries"
