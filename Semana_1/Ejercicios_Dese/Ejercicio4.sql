63211147882511b652a6693a076d72f9ac71f919
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:02:32 -0400

    Update Ejercicio4.sql
    Descripcion del problema

--- Semana_1/Ejercicios_Dese/Ejercicio4.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio4.sql
@@ -1,3 +1,9 @@
+--## 4.sql
+--Algunas ciudades tienen más escuelas públicas que otras. En `4.sql`, escribe una consulta SQL 
+--para encontrar las 10 ciudades con más escuelas públicas. Tu consulta debe devolver los nombres
+--de las ciudades y el número de escuelas públicas dentro de ellas, ordenadas de mayor a menor 
+--número de escuelas públicas. Si dos ciudades tienen el mismo número de escuelas públicas, ordénalas alfabéticamente.
+
 SELECT "city" ,COUNT("city") FROM "schools"
 WHERE "type" = 'Public School'
 GROUP BY "city"
