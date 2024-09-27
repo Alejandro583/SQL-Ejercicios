db03be5524d9cfe23719f6eed099bbdbca64cd8a
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:56:43 -0400

    Update Ejercicio9.sql

--- Semana_0/36_Views/Ejercicio9.sql
+++ Semana_0/36_Views/Ejercicio9.sql
@@ -1,2 +1,4 @@
+-- 9.sql: Encuentra el título en inglés y el artista de la impresión con el brillo más alto.
+
 SELECT "english_title","artist" FROM "views"
 WHERE "brightness" = (SELECT MAX("brightness") FROM "views");
