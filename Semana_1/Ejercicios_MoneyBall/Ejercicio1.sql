6b381135c5981014f65ee6d5282db3c327db377c
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:28:17 -0400

    Update Ejercicio1.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio1.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio1.sql
@@ -1,3 +1,13 @@
+--## 1.sql
+--Comienza obteniendo una idea de cómo han cambiado los salarios promedio 
+--de los jugadores a lo largo del tiempo. En `1.sql`, escribe una consulta 
+--SQL para encontrar el salario promedio de los jugadores por año.
+
+-- Ordena por año en orden descendente.
+-- Redondea el salario a dos decimales y llama a la columna “average salary”.
+-- Tu consulta debe devolver una tabla con dos columnas: una para el año y otra para el salario promedio.
+
+
 SELECT "year" ,ROUND(AVG("salary"),2) AS 'AVG Salary'
 FROM "salaries"
 GROUP BY "year"
