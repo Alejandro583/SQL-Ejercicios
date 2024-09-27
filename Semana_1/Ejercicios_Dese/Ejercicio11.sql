a0a4b8d61818398fbf16198a796c40fcb4ef3137
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:10:14 -0400

    Update Ejercicio11.sql

--- Semana_1/Ejercicios_Dese/Ejercicio11.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio11.sql
@@ -1,3 +1,9 @@
+--## 11.sql
+--¿Existe una relación entre los gastos escolares y las tasas de graduación?
+--En `11.sql`, escribe una consulta SQL para mostrar los nombres de las escuelas,
+--su gasto por estudiante y su tasa de graduación. Ordena las escuelas de mayor a menor gasto por estudiante.
+--Si dos escuelas tienen el mismo gasto por estudiante, ordénalas por nombre.
+
 SELECT "schools"."name","expenditures"."per_pupil_expenditure"
 ,"graduation_rates"."graduated"
 FROM "schools"
