d4291a52bea297b0aeb32dd2d291110558e7b54f
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 15:08:08 -0400

    Update Ejercicio10.sql

--- Semana_0/Ejercicios_Cyberchase/Ejercicio10.sql
+++ Semana_0/Ejercicios_Cyberchase/Ejercicio10.sql
@@ -1,3 +1,5 @@
+-- ejercicio10: Consultas que enumeran identificadores, títulos y códigos de producción de todos los episodios.
+
 SELECT "id", "title","production_code"
 FROM "episodes"
 ORDER BY "production_code";
