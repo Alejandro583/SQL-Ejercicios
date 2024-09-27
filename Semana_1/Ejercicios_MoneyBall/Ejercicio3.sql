0bbf0582dd297c726380123cf4dfabb25b03eb5f
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:31:05 -0400

    Update Ejercicio3.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio3.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio3.sql
@@ -1,3 +1,12 @@
+--## 3.sql
+--Tu equipo va a necesitar un gran bateador de home runs. Ken Griffey Jr., 
+--un galardonado Silver Slugger y Gold Glove de mucho tiempo,
+--podría ser un buen prospecto. En `3.sql`, escribe una consulta SQL 
+--para encontrar el historial de home runs de Ken Griffey Jr.
+-- Ordena por año en orden descendente.
+--Nota que puede haber dos jugadores con el nombre "Ken Griffey". Este Ken Griffey nació en 1969.
+-- Tu consulta debe devolver una tabla con dos columnas: una para el año y otra para los home runs.
+
 SELECT "performances"."year","performances"."HR"
 FROM "performances" JOIN "players"
 ON "players"."id" = "performances"."player_id"
