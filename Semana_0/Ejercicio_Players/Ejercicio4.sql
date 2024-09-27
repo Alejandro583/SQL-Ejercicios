a8fc92cd3ae1fd09e073181ae5963e2ac99b8120
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:49:38 -0400

    Update Ejercicio4.sql

--- Semana_0/Ejercicio_Players/Ejercicio4.sql
+++ Semana_0/Ejercicio_Players/Ejercicio4.sql
@@ -1,3 +1,6 @@
+-- 4.sql: Encuentra el nombre y apellidos de los jugadores no nacidos en los Estados Unidos,
+-- ordenando alfabéticamente por nombre y luego por apellido.
+
 SELECT "first_name", "last_name" FROM "players"
 WHERE "birth_country" != 'USA'
 ORDER BY "first_name","last_name";
