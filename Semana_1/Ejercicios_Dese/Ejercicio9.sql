74c8e871256146631a705d264d36bb13b9f310ae
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:07:36 -0400

    Update Ejercicio9.sql

--- Semana_1/Ejercicios_Dese/Ejercicio9.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio9.sql
@@ -1,3 +1,8 @@
+--## 9.sql
+--Otro padre quiere enviar a su hijo a un distrito con pocos otros estudiantes.
+--En `9.sql`, escribe una consulta SQL para encontrar el nombre (o los nombres) 
+--del distrito escolar con el menor número de estudiantes. Informa solo el/los nombre(s).
+
 SELECT "districts"."name" FROM "districts"
 JOIN "expenditures"
 ON "districts"."id" = "expenditures"."district_id"
