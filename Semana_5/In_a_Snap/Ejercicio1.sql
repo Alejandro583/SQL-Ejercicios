8c67058da058e3897bccc714ef5f0824411470ed
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:21:41 -0400

    Update Ejercicio1.sql

--- Semana_5/In_a_Snap/Ejercicio1.sql
+++ Semana_5/In_a_Snap/Ejercicio1.sql
@@ -1,2 +1,7 @@
+-- 1.sql
+-- El equipo de compromiso de usuarios de la aplicación necesita identificar a los usuarios activos. 
+-- Encuentra todos los nombres de usuario de los usuarios que han iniciado sesión desde 2024-01-01. 
+-- Asegúrate de que tu consulta utilice el índice search_users_by_last_login, que se define de la siguiente manera:
+
 SELECT "username" FROM "users"
 WHERE "last_login_date" >= '2024-01-01';
