64524fc94a4d391ec21e2ffec51db1e9d90b0c85
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:57:31 -0400

    Update Ejercicio10.sql

--- Semana_0/36_Views/Ejercicio10.sql
+++ Semana_0/36_Views/Ejercicio10.sql
@@ -1,3 +1,7 @@
+-- 10.sql: Escribe una consulta SQL que responda a una pregunta de tu elección sobre las impresiones,
+-- utilizando AS para renombrar una columna, incluyendo al menos una condición WHERE 
+-- y ordenando por al menos una columna con ORDER BY.
+
 SELECT "english_title" AS 'Title',"print_number" AS 'Print Number'
 FROM "views"
 WHERE "Entropy" > 7.5 ORDER BY "print_number";
