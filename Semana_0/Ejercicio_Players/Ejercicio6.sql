44ce2c36b87abcea134a0addf97ef1a1bdad3d2e
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:50:38 -0400

    Update Ejercicio6.sql

--- Semana_0/Ejercicio_Players/Ejercicio6.sql
+++ Semana_0/Ejercicio_Players/Ejercicio6.sql
@@ -1,3 +1,6 @@
+-- 6.sql: Devuelve nombre, apellido y fecha de debut de los jugadores nacidos en Pittsburgh, PA,
+-- ordenados por fecha de debut (de la más reciente a la más antigua) y alfabéticamente.
+
 SELECT "first_name", "last_name", "debut"
 FROM "players"
 WHERE "birth_city" = 'Pittsburgh'
