ed49af57eaf3957f369219e674702a242ddb44fd
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:51:10 -0400

    Update Ejercicio7.sql

--- Semana_0/Ejercicio_Players/Ejercicio7.sql
+++ Semana_0/Ejercicio_Players/Ejercicio7.sql
@@ -1,3 +1,6 @@
+-- 7.sql: Cuenta el número de jugadores que batean con la mano derecha
+-- y lanzan con la mano izquierda, o viceversa.
+
 SELECT COUNT("first_name") FROM "players"
 WHERE ("bats" = 'R' AND "throws" = 'L')
 OR ("bats" = 'L' AND "throws" = 'R');
