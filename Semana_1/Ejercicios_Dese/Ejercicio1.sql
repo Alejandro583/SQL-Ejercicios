f409ebc812980a80673d775d859ece51ab5e291f
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 09:58:12 -0400

    Update Ejercicio1.sql
    Agregando descripción del problema

--- Semana_1/Ejercicios_Dese/Ejercicio1.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio1.sql
@@ -1,2 +1,7 @@
+--## 1.sql
+-- Tu colega está preparando un mapa de todas las escuelas públicas en Massachusetts. En `1.sql`, 
+-- escribe una consulta SQL para encontrar los nombres y las ciudades de todas las escuelas públicas en Massachusetts.
+-- Ten en cuenta que no todas las escuelas en la tabla `schools` se consideran escuelas públicas tradicionales.
+-- Massachusetts también reconoce las escuelas charter, que (según DESE) se consideran distintas.
 SELECT "name", "city" FROM "schools"
 WHERE "type" = 'Public School';
