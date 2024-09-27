c50992f711cd0d2ea2ef465c62394bd3f2069177
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:23:28 -0400

    Update Ejercicio3.sql

--- Semana_5/In_a_Snap/Ejercicio3.sql
+++ Semana_5/In_a_Snap/Ejercicio3.sql
@@ -1,3 +1,10 @@
+-- La aplicación necesita clasificar a los "mejores amigos" de un usuario, similar a la función "Emojis de amigos" de Snapchat. 
+-- Encuentra los ID de usuario de los 3 principales usuarios a los que creativewisdom377 envía mensajes con más frecuencia. 
+-- Ordena los ID de usuario por el número de mensajes que creativewisdom377 ha enviado a esos usuarios, de más a menos.
+ 
+-- Asegúrate de que tu consulta utilice el índice search_messages_by_from_user_id, que se define de la siguiente manera:
+
+
 SELECT "to_user_id" FROM "messages"
 WHERE "from_user_id" = (SELECT "id" FROM "users"
      WHERE "username" = 'creativewisdom377')
