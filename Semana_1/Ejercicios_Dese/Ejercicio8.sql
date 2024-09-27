2f8c5b974d28f8660246a45f05181dca0252a71c
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:06:53 -0400

    Update Ejercicio8.sql

--- Semana_1/Ejercicios_Dese/Ejercicio8.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio8.sql
@@ -1,3 +1,8 @@
+--## 8.sql
+--Un padre quiere enviar a su hijo a un distrito con muchos otros estudiantes. 
+--En `8.sql`, escribe una consulta SQL para mostrar los nombres de todos los 
+--distritos escolares y el número de estudiantes matriculados en cada uno.
+
 SELECT "districts"."name","expenditures"."pupils" FROM "districts"
 JOIN "expenditures"
 ON "districts"."id" = "expenditures"."district_id";
