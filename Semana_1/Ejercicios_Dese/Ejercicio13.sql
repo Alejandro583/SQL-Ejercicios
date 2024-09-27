37cfd5041acc782df134bc81eb6322c2713dad81
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:12:54 -0400

    Update Ejercicio13.sql

--- Semana_1/Ejercicios_Dese/Ejercicio13.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio13.sql
@@ -1,3 +1,7 @@
+--## 13.sql
+--En `13.sql`, escribe una consulta SQL para responder una pregunta que tengas sobre los datos. La consulta debe:
+--Incluir al menos un JOIN o una subconsulta.
+
 SELECT "districts"."name","expenditures"."pupils" FROM "districts"
 JOIN "expenditures"
 ON "districts"."id" = "expenditures"."district_id"
