af1ac904927272cbc85cc85dbc76e3276f755b4d
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:55:57 -0400

    Update Ejercicio7.sql

--- Semana_0/36_Views/Ejercicio7.sql
+++ Semana_0/36_Views/Ejercicio7.sql
@@ -1,3 +1,5 @@
+-- 7.sql: Enumera los títulos en inglés de las 5 impresiones más brillantes de Hiroshige.
+
 SELECT "english_title" FROM "views"
 WHERE "artist" = 'Hiroshige'
 ORDER BY "brightness" DESC LIMIT 5;
