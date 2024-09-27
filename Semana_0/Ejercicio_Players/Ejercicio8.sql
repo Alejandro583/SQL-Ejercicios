69bcb909d8e26f5145275132a3e6ce55a52094e4
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:51:39 -0400

    Update Ejercicio8.sql

--- Semana_0/Ejercicio_Players/Ejercicio8.sql
+++ Semana_0/Ejercicio_Players/Ejercicio8.sql
@@ -1,3 +1,6 @@
+-- 8.sql: Encuentra la altura y peso promedio (redondeados a dos decimales) de los jugadores 
+-- que debutaron a partir del 1 de enero de 2000. Devuelve columnas con los nombres “Average Height” y “Average Weight”.
+
 SELECT ROUND(AVG("height"),2) AS 'Average Height',ROUND(AVG("weight"),2) AS 'Average Weight'
 FROM "players"
 WHERE "debut" >= '2000-01-01';
