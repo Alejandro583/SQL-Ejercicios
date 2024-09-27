c5839d4d042b84bd9476fe4c2d3cc591b6957f8c
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 13:33:23 -0400

    Update available.sql

--- Semana_4/Bed_and_Breakfast/available.sql
+++ Semana_4/Bed_and_Breakfast/available.sql
@@ -1,3 +1,13 @@
+-- En available.sql, escribe una declaración SQL para crear una vista llamada available.
+-- Esta vista debe contener todas las fechas que están disponibles en todas las listas.
+-- Asegúrate de que la vista contenga las siguientes columnas:
+
+-- id, que es el ID de la lista de la tabla listings.
+-- property_type, de la tabla listings.
+-- host_name, de la tabla listings.
+-- date, de la tabla availabilities, que es la fecha de disponibilidad.
+
+
 CREATE VIEW "available" AS
 SELECT "listings"."id","properrty_type","host_name","date" FROM "listings"
 JOIN "availabilities" ON "listings"."id" = "availabilities"."listing_id"
