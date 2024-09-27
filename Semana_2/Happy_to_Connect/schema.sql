e94ba4eda49f9175e3bad0655003e348961087c7
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 11:59:59 -0400

    Update schema.sql

--- Semana_2/Happy_to_Connect/schema.sql
+++ Semana_2/Happy_to_Connect/schema.sql
@@ -1,3 +1,69 @@
+-- Tu tarea consiste en crear una base de datos SQLite para LinkedIn desde cero, 
+-- mediante la escritura de un conjunto de sentencias CREATE TABLE en un archivo schema.sql. 
+-- Los detalles de implementación son a tu elección, aunque debes asegurarte, como mínimo, 
+-- de que tu base de datos cumpla con las especificaciones de la plataforma y que pueda 
+-- representar los datos de muestra proporcionados.
+
+-- Plataforma
+
+-- Usuarios
+-- El corazón de la plataforma de LinkedIn son sus personas. 
+-- Tu base de datos debe ser capaz de representar la siguiente información sobre los usuarios de LinkedIn:
+-- - Su nombre y apellido
+-- - Su nombre de usuario
+-- - Su contraseña
+-- Ten en cuenta que, si una empresa sigue las mejores prácticas, 
+-- las contraseñas de las aplicaciones están "hasheadas". 
+-- No necesitas preocuparte por hashear contraseñas aquí.
+
+-- Escuelas y Universidades
+-- LinkedIn también permite cuentas oficiales de escuelas o universidades, 
+-- como la de Harvard, para que los exalumnos (es decir, aquellos que han asistido) 
+-- puedan identificar su afiliación. Asegúrate de que la base de datos de LinkedIn 
+-- pueda almacenar la siguiente información sobre cada escuela:
+-- - El nombre de la escuela
+-- - El tipo de escuela (por ejemplo, "Escuela Primaria", "Escuela Secundaria", 
+-- "Escuela Preparatoria", "Escuela Inferior", "Escuela Superior", "Colegio", "Universidad", etc.)
+-- - La ubicación de la escuela
+-- - El año en que se fundó la escuela
+
+-- Empresas
+-- LinkedIn permite a las empresas crear sus propias páginas, 
+-- como la de LinkedIn, para que los empleados puedan identificar su empleo 
+-- pasado o actual con la empresa. Asegúrate de que la base de datos de LinkedIn 
+-- pueda almacenar la siguiente información para cada empresa:
+-- - El nombre de la empresa
+-- - La industria de la empresa (por ejemplo, "Educación", "Tecnología", "Finanzas", etc.)
+-- - La ubicación de la empresa
+
+-- Conexiones
+-- Y finalmente, la esencia de LinkedIn es su capacidad para facilitar 
+-- conexiones entre personas. Asegúrate de que la base de datos de LinkedIn 
+-- pueda soportar cada una de las siguientes conexiones.
+
+-- Conexiones con Personas
+-- La base de datos de LinkedIn debe poder representar conexiones mutuas 
+-- (recíprocas, bidireccionales) entre usuarios. 
+-- No necesitas preocuparte por conexiones unidireccionales, 
+-- como que el usuario A "siga" al usuario B sin que el usuario B "siga" al usuario A.
+
+-- Conexiones con Escuelas
+-- Un usuario debe poder crear una afiliación con una determinada escuela. 
+-- Y de manera similar, esa escuela debería poder encontrar a sus exalumnos. 
+-- Además, permite que un usuario defina:
+-- - La fecha de inicio de su afiliación (es decir, cuando comenzó a asistir a la escuela)
+-- - La fecha de finalización de su afiliación (es decir, cuando se graduó), si aplica
+-- - El tipo de grado obtenido/pursued (por ejemplo, "BA", "MA", "PhD", etc.)
+
+-- Conexiones con Empresas
+-- Un usuario debe poder crear una afiliación con una determinada empresa. 
+-- Y de manera similar, una empresa debería poder encontrar a sus empleados actuales y pasados. 
+-- Además, permite que un usuario defina:
+-- - La fecha de inicio de su afiliación (es decir, la fecha en que comenzó a trabajar con la empresa)
+-- - La fecha de finalización de su afiliación (es decir, cuando dejó la empresa), si aplica
+-- - El título que tenía mientras estaba afiliado a la empresa
+
+
 CREATE TABLE "usuarios"(
     "id" NUMERIC,
     "nombre_usuario" TEXT,
