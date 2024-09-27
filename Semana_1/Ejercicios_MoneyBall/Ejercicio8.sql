1e72d4b389bdd6edc492a9df84b361678e36fe77
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:43:19 -0400

    Update Ejercicio8.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio8.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio8.sql
@@ -1,13 +1,17 @@
+--## 8.sql
+--¿Cuánto necesitarían pagar los A's para obtener al mejor bateador de home runs 
+--de la temporada pasada? En `8.sql`, escribe una consulta SQL para encontrar 
+--el salario de 2001 del jugador que conectó la mayor cantidad de home runs en 2001.
+-- Tu consulta debe devolver una tabla con una columna, el salario del jugador.
+
 SELECT "salaries"."salary"
 FROM "salaries"
 JOIN "performances"
 ON "salaries"."player_id" = "performances"."player_id"
 WHERE "performances"."HR" = (
-    SELECT MAX("performances"."HR") FROM "performances"
-    WHERE "performances"."year" = 2001
-    ORDER BY MAX("performances"."HR") DESC
-    LIMIT 1
+    SELECT MAX("HR") 
+    FROM "performances"
+    WHERE "year" = 2001
 ) AND "performances"."year" = 2001
-AND "salaries"."salary" != 10658826
 ORDER BY "salaries"."salary" DESC
 LIMIT 1;
