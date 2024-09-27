6d836b7580b1b969141064e8d973cf1bbb79b236
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 15:09:08 -0400

    Update Ejercicio13.sql

--- Semana_0/Ejercicios_Cyberchase/Ejercicio13.sql
+++ Semana_0/Ejercicios_Cyberchase/Ejercicio13.sql
@@ -1,3 +1,5 @@
+-- ejercicio13: Consultas para explorar una pregunta de elección.
+
 SELECT "title", "topic" FROM "episodes"
 WHERE "air_date" BETWEEN '2002-01-01'
 AND '2023-12-31' AND "topic" IS NOT NULL;
