806156007e845b45eee4576cac3b1e37dd101f80
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:55:37 -0400

    Update Ejercicio6.sql

--- Semana_0/36_Views/Ejercicio6.sql
+++ Semana_0/36_Views/Ejercicio6.sql
@@ -1,3 +1,5 @@
+-- 6.sql: Calcula la entropía promedio de las impresiones de Hiroshige, redondeada a dos decimales.
+
 SELECT ROUND(AVG("entropy"),2) AS 'Hiroshige Average Entropy'
 FROM "views" WHERE "artist" = 'Hiroshige';
 
