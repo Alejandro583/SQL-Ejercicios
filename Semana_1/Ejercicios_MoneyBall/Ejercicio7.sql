3e1cb74a58f26ac431378bc04d644294e59e5db1
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:35:07 -0400

    Update Ejercicio7.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio7.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio7.sql
@@ -1,3 +1,9 @@
+--## 7.sql
+--Necesitas hacer una recomendación sobre qué jugador (o jugadores) evitar reclutar. 
+--En `7.sql`, escribe una consulta SQL para encontrar el nombre del jugador
+--que ha recibido el salario más alto de todos los tiempos en la Major League Baseball.
+--Tu consulta debe devolver una tabla con dos columnas: una para el primer nombre del jugador y otra para su apellido.
+
 SELECT "players"."first_name", "players"."last_name"
 FROM "players" JOIN "salaries"
 ON "players"."id" = "salaries"."player_id"
