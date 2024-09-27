06eed2331eff7231aad3716f6987a67c5f836475
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:33:19 -0400

    Update Ejercicio5.sql

--- Semana_1/Ejercicios_MoneyBall/Ejercicio5.sql
+++ Semana_1/Ejercicios_MoneyBall/Ejercicio5.sql
@@ -1,3 +1,8 @@
+--## 5.sql
+--Es un día algo lento en la oficina. Aunque Satchel ya no juega, en `5.sql`, 
+--escribe una consulta SQL para encontrar todos los equipos en los que Satchel Paige jugó.
+--Tu consulta debe devolver una tabla con una única columna, con el nombre de los equipos.
+
 SELECT "teams"."name" FROM "teams"
 JOIN "performances"
 ON "teams"."id" = "performances"."team_id"
