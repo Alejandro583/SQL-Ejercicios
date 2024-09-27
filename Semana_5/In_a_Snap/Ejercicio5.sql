1b61ed3bc6cbefccea6d68267bcfb56787b61693
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:24:55 -0400

    Update Ejercicio5.sql

--- Semana_5/In_a_Snap/Ejercicio5.sql
+++ Semana_5/In_a_Snap/Ejercicio5.sql
@@ -1,3 +1,10 @@
+-- Para cualquier par de usuarios, la aplicación necesita mostrar rápidamente una lista de los amigos que tienen en común. 
+-- Dado dos nombres de usuario, lovelytrust487 y exceptionalinspiration482, encuentra los ID de usuario de sus amigos mutuos. 
+-- Un amigo mutuo es un usuario que tanto lovelytrust487 como exceptionalinspiration482 cuentan entre sus amigos.
+ 
+-- Asegúrate de que tu consulta utilice el índice que se crea automáticamente en las columnas de clave primaria de la tabla friends. 
+-- Este índice se llama sqlite_autoindex_friends_1.
+
 SELECT "friend_id" FROM "friends"
      WHERE  "user_id" = (SELECT "id" FROM "users"
          WHERE "username" = "lovelytrust487")
