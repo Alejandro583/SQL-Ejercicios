386c1a8e8f0fcf10ef23b2dec3bb3d6d00bba7d6
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 14:50:02 -0400

    Update Ejercicio5.sql

--- Semana_0/Ejercicio_Players/Ejercicio5.sql
+++ Semana_0/Ejercicio_Players/Ejercicio5.sql
@@ -1,2 +1,5 @@
+-- 5.sql: Devuelve el nombre y apellido de todos los bateadores diestros,
+-- ordenados alfabéticamente.
+
 SELECT "first_name", "last_name" FROM "players"
 WHERE "bats" = 'R' ORDER BY "first_name","last_name";
