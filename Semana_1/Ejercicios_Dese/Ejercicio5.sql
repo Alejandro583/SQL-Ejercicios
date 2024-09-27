44ee188e43d1f517cf0a35df44188c94ef8a8068
Juan Alejandro López Meza <145209038+Alejandro583@users.noreply.github.com>
2024-09-27 10:03:35 -0400

    Update Ejercicio5.sql
    Descripcion del problema

--- Semana_1/Ejercicios_Dese/Ejercicio5.sql
+++ Semana_1/Ejercicios_Dese/Ejercicio5.sql
@@ -1,3 +1,10 @@
+--## 5.sql
+--DESE quiere que determines en qué ciudades podrían necesitarse más escuelas públicas. 
+--En `5.sql`, escribe una consulta SQL para encontrar ciudades con 3 o menos escuelas públicas. 
+--Tu consulta debe devolver los nombres de las ciudades y el número de escuelas públicas dentro de ellas, 
+--ordenadas de mayor a menor número de escuelas públicas. Si dos ciudades tienen el mismo número de escuelas públicas,
+--ordénalas alfabéticamente.
+
 SELECT city, COUNT(*) AS num_schools
 FROM schools
 WHERE type = 'Public School'
